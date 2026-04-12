9a2374b347963fc772b0575a27a09c311cc0c26f
yUto <kyawnyeinei19@gmail.com>
2026-04-12 23:21:05 +0630

    update index.html

--- javascript/tab.js
+++ javascript/tab.js
@@ -1,14 +1,5 @@
-function switchPage(id) {
+  function switchPage(id) {
             const name = document.getElementById('ras');
-            const value = name.value;
-            if (value !== "") {
-                name.selectedIndex = 0;
-                window.location.href = value;
-
-            }
-        }
-function findpage(id) {
-            const name = document.getElementById('search');
             const value = name.value;
             if (value !== "") {
                 name.selectedIndex = 0;
